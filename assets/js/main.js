5f4849b4cc63a3ddcf358312d328848935d07ff7
unknown <Meegod>
2022-12-19 15:17:38 +0100

    latest commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,7 +85,7 @@
   if (selectHeader) {
     const headerScrolled = () => {
       if (window.scrollY > 100) {
-        selectHeader.classList.add('header-scrolled')
+        // selectHeader.classList.add('header-scrolled')
       } else {
         selectHeader.classList.remove('header-scrolled')
       }
